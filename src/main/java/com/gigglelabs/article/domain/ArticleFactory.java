ac619f1355a211a2f5ad8b99774b49f0641097d8
zoster0828 <zoster0828@gmail.com>
2024-02-21 16:41:32 +0900

    :sparkle: feat : Id 생성 로직을 url hashing 한 값으로 변경

--- src/main/java/com/gigglelabs/article/domain/ArticleFactory.java
+++ src/main/java/com/gigglelabs/article/domain/ArticleFactory.java
@@ -1,29 +1,12 @@
 package com.gigglelabs.article.domain;
 
 import com.gigglelabs.article.application.dto.ArticleDto;
+import com.gigglelabs.article.port.dto.SiteDefaultInfo;
 
-import java.util.Date;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class ArticleFactory {
-    public static Article create(String title, String url, String site, Long likes, Long views, Long date) {
-        return Article.builder()
-                .id(generateId(site))
-                .title(title)
-                .url(url)
-                .sites(Sites.valueOf(site))
-                .likes(0L)
-                .views(0L)
-                .sourceLikes(likes)
-                .sourceViews(views)
-                .sourceDate(date)
-                .thumbnailUrl("")
-                .build();
-    }
-
-    private static String generateId(String site) {
-        return System.currentTimeMillis()+"_"+site;
-    }
-
     public static Article create(ArticleDto articleDto) {
         return Article.builder()
                 .id(articleDto.id)
@@ -37,5 +20,40 @@ public class ArticleFactory {
                 .sourceDate(articleDto.sourceDate)
                 .thumbnailUrl(articleDto.thumbnailUrl)
                 .build();
+    }
+
+    public static Article create(String site,SiteDefaultInfo siteDefaultInfo) {
+        return Article.builder()
+                .id(generateId(siteDefaultInfo.url))
+                .title(siteDefaultInfo.title)
+                .url(siteDefaultInfo.url)
+                .sites(Sites.valueOf(site))
+                        .likes(0L)
+                        .views(0L)
+                        .sourceLikes(siteDefaultInfo.likes)
+                        .sourceViews(siteDefaultInfo.views)
+                        .sourceDate(siteDefaultInfo.date.getTime())
+                        .thumbnailUrl("")
+                        .build();
+    }
+
+    public static String generateId(String url) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+
+            byte[] hashBytes = digest.digest(url.getBytes());
+
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : hashBytes) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+
+            return hexString.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Error hashing the URL", e);
+        }
     }
 }
